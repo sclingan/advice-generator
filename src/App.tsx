5d2ea63bf448d5ffd3526ecb1ebe20aba343602f
Sclingan <sclingan1@gmail.com>
2024-02-18 19:45:15 -0600

    Added api call, variables and state

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import './App.css'
 import dividerMobile from '../public/images/pattern-divider-mobile.svg'
 import dice from '../public/images/icon-dice.svg';
 
 function App() {
 
-  const [advice, setAdvice] = useState();
+  const [advice, setAdvice] = useState('hello');
+  const [id, setId] = useState(1);
   const url = 'https://api.adviceslip.com/advice'
 
-  useEffect(() => {
-    fetch(url).then(res => {
-      const response = res.json();
-      JSON.stringify(response);
-      console.log(response);
-      // console.log(res);
-    }).then(result => {
-      console.log(result);
-      // setAdvice(data);
-    })
-  }, []);
+  
+    async function getAdvice(){
+      const response = await fetch(url);
+      const text = await response.json();
+      setAdvice(text.slip.advice);
+      setId(text.slip.id);
+    }
+  
   return (
     <main>
       <header>
         <h1 className='sr-only'>Advice Generator</h1>
-        <p>Advice #</p>
+        <p>Advice # {id}</p>
       </header>
       <section>
         <p>"{advice}"</p>
         <img src={dividerMobile} alt=""></img>
         <div className='icon-wrapper'>
-        <img src={dice} alt="" className='icon'></img>
+        <button onClick={getAdvice}><img src={dice} alt="" className='icon'></img></button>
         </div>
       </section>
    
